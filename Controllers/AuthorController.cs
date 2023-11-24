c57ce1428c0a8cb642c66f21f2cf5560576b9e73
Timbro <alexei.timbro@gmail.com>
2023-11-23 20:25:29 -0500

    Authentication For Authors
    Making authors only accessible when logged in to the website.

--- Controllers/AuthorController.cs
+++ Controllers/AuthorController.cs
@@ -1,10 +1,12 @@
 ﻿using BookStoreApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 namespace BookStoreApp.Controllers
 {
+    [Authorize]
     public class AuthorController : Controller
     {
         private BookstoreContext context { get; set; }
